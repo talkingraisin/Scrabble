a16b07041d0b1ef5eed348b151c907f08178b9c6
Katherine <klfive@princeton.edu>
2012-01-31 17:22:56 -0500

    changed Square

--- Square.java
+++ Square.java
@@ -2,4 +2,20 @@ public class Square
 {
     private int special; // DW, TW, DL, TL
     private Tile tile;
+
+    public Square(Tile tile, int special)
+    {
+    	this.tile = tile;
+	this.special = special;
+    }
+
+    public Tile getTile()
+    {
+    	return tile;
+    }
+
+    public int getSpecial()
+    {
+    	return special;
+    }
 }
